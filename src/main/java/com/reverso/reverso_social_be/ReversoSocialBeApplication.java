aea72e964442a20e4f98a474565cc55f0ad51177
AngelaBello-creator <angelabellomedina@gmail.com>
2025-11-28 10:05:32 +0100

    feat: clean initial backend setup from Spring Initializr

---
+++ src/main/java/com/reverso/reverso_social_be/ReversoSocialBeApplication.java
@@ -0,0 +1,13 @@
+package com.reverso.reverso_social_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReversoSocialBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReversoSocialBeApplication.class, args);
+	}
+
+}
