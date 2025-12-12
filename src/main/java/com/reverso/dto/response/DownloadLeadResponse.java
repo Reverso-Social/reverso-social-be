ab11f2e2472cbba205ee4250916e4add524de8f4
Luisa Moreno <lumoreno323@gmail.com>
2025-12-12 08:43:39 +0100

    The Download Lead entity is created where name and email data are collected when requesting to download a resource

---
+++ src/main/java/com/reverso/dto/response/DownloadLeadResponse.java
@@ -0,0 +1,22 @@
+package com.reverso.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DownloadLeadResponse {
+    private UUID id;
+    private String name;
+    private String email;
+    private UUID resourceId;
+    private String resourceTitle;
+    private LocalDateTime createdAt;
+}
